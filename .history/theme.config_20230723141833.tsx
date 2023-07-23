ef8ef7e1286ab4942af2f6b9cdc2f7d633b98dc3
i-ice-bear <andydowsen47us@gmail.com>
2023-07-23 14:34:41 +0530

    Project have been customised to default codebase

---
+++ .history/theme.config_20230723141833.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>LinkHive Tars. Dev.</span>,
+  project: {
+    link: 'https://github.com/shuding/nextra-docs-template',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/i-ice-bear/',
+  footer: {
+    text: 'Documentation of the Tars. Desktop including how to operate it and many more.',
+  },
+}
+
+export default config
--- .history/theme.config_20230723142425.tsx
+++ .history/theme.config_20230723142425.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
